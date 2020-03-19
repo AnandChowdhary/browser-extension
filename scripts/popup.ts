88d2c14d8c9ea12d8b6c200e003980d288e10dbc
Anand Chowdhary <mail@anandchowdhary.com>
2020-03-19 18:21:44 +0530

    :heavy_plus_sign: Add parcel for building

--- scripts/popup.ts
+++ scripts/popup.ts
@@ -5,13 +5,22 @@ const html = `<!DOCTYPE html>
 <html>
   <head>
     <title>Browser Extension</title>
+    <link rel="stylesheet" href="app.scss" />
   </head>
   <body>
     <p>Hello</p>
-    <script src="popup.ts"></script>
+    <script src="app.ts"></script>
   </body>
 </html>`;
 
 (async () => {
+  await writeFile(
+    join(__dirname, "..", "static", "app.scss"),
+    join(__dirname, "..", "dist", "app.scss")
+  );
+  await writeFile(
+    join(__dirname, "..", "scripts", "app.ts"),
+    join(__dirname, "..", "dist", "app.ts")
+  );
   await writeFile(join(__dirname, "..", "dist", "popup.html"), html);
 })();
