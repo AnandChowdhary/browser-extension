8e5ee5c2cf4ef147d4b2ea8e8a105ecf9b7fc971
Anand Chowdhary <mail@anandchowdhary.com>
2020-03-19 17:49:21 +0530

    :sparkles: Add manifest generation

--- scripts/manifest.ts
+++ scripts/manifest.ts
@@ -1,27 +1,32 @@
-import { mkdirp } from "fs-extra";
+import { mkdirp, writeJson, readJson } from "fs-extra";
 import { join } from "path";
 
-const manifest = `{
-  "manifest_version": 2,
-  "name": "NAME",
-  "description": "DESCRIPTION",
-  "version": "1.0.0",
-  "browser_action": {
-    "default_icon": "icon16.png",
-    "default_popup": "index.html"
+const manifest = {
+  manifest_version: 2,
+  name: "",
+  description: "",
+  version: "0.0.0",
+  browser_action: {
+    default_icon: "icon16.png",
+    default_popup: "index.html"
   },
-  "background": {
-    "scripts": ["dist/background.js"],
-    "persistent": false
+  background: {
+    scripts: ["dist/background.js"],
+    persistent: false
   },
-  "icons": {
+  icons: {
     "16": "icon16.png",
     "48": "icon48.png",
     "128": "icon128.png"
   },
-  "permissions": []
-}`;
+  permissions: []
+};
 
 (async () => {
   await mkdirp(join(__dirname, "..", "dist"));
+  const pkg = await readJson(join(__dirname, "..", "package.json"));
+  manifest.name = pkg.name;
+  manifest.description = pkg.description;
+  manifest.version = pkg.version;
+  await writeJson(join(__dirname, "..", "dist", "manifest.json"), manifest);
 })();
